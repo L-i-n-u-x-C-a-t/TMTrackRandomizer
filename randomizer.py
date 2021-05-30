cc87e22ef2ce4d7c51d16db8c0c5ab0195247e8c
LinuxCat <masch77.linuxcat@gmail.com>
2021-05-30 11:33:16 +0200

    Update randomizer.py

--- randomizer.py
+++ randomizer.py
@@ -7,6 +7,8 @@ start = int(input("Type of start (Normal = 0, multilap = 1) : "))
 output = name + ".Challenge.Gbx"
 template = "Template.Challenge.Gbx"
 trackdata = []
+cplist = [13,20,45,46,72,73,114,115,116,117,118,119,120,200,208] #used for CP counter.
+cpcount = 0
 for i in range(loop):
     block = random.randint(1, 292)
     if block == 210: #workaround due to a bug in pygbx
@@ -14,6 +16,7 @@ for i in range(loop):
     if block != 14: #avoid putting accidental start points
         if block != 16:
             trackdata.append((block,random.randint(0,31), random.randint(0,31), random.randint(0,31), random.randint(0,3)))
+    if block in cplist:cpcount+=1
 #track data format : [(block, x,y,z,rotation),(...)]
 if start == 1:
     trackdata.append((16,random.randint(0,31), random.randint(0,31), random.randint(0,31), random.randint(0,3)))
@@ -22,4 +25,4 @@ else:
 
 options = {"track_data":trackdata, "map_name":name}
 save_gbx(options, template, output)
-print("Done!")
+print("Done!\nNumber of checkpoints : "+str(cpcount))
